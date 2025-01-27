909cda63a92a9d328e14cbd96c16b63c92c3a554
forest <kazushima1702@gmail.com>
2025-01-27 15:12:52 +0900

    cart page

--- src/app/orders/cart/page.tsx
+++ src/app/orders/cart/page.tsx
@@ -1,4 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabaseClient";
+import { useRouter } from "next/navigation";
+import Header from "@/app/_components/Header";
+//import { Order } from "@/app/types/order";
+
 export default function CartPage() {
-  return <div></div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      if (!session) {
+        // 未認証の場合はログインページにリダイレクト
+        router.push("/login");
+      } /*else {
+          fetchOrders();
+        }*/
+    };
+
+    checkUser();
+  }, [router]);
+
+  return <div>カートのページ</div>;
 }
-// ↑消してよい
